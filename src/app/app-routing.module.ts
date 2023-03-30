5b04a4f70906bba099390b489596f9a2f93f108e
peeteko <koen.peeters@bpost.be>
2023-03-30 16:10:47 +0200

    plugins route for plugins

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'plugins' , loadChildren : () => import('mfeEpfPlugins/Module').then(m => m.PluginsHomeModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
